8020cda2ec85740584d54f6dca5105602b237e6a
Anselmo <anselmo.cstecnologia@gmail.com>
2023-09-21 17:13:02 -0300

    adicionando grafico no dashboard

--- financeiro/models.py
+++ financeiro/models.py
@@ -6,8 +6,8 @@ from django.db import models
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField(blank=True)
+    name = models.CharField(max_length=100, verbose_name="Nome")
+    description = models.TextField(blank=True, verbose_name="Descrição")
 
     def __str__(self):
         return self.name
@@ -19,11 +19,17 @@ class Transaction(models.Model):
         ("Receita", "Receita"),
     ]
 
-    description = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_date = models.DateField()
-    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    description = models.CharField(
+        max_length=255, verbose_name="Descrição da Transação"
+    )
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, verbose_name="Categoria"
+    )
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Valor")
+    transaction_date = models.DateField(verbose_name="Data do Pagamento")
+    transaction_type = models.CharField(
+        max_length=10, choices=TRANSACTION_TYPES, verbose_name="Tipo da Transação"
+    )
 
     def __str__(self):
         return self.description
--- financeiro/views.py
+++ financeiro/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     TemplateView,
@@ -7,8 +7,16 @@ from django.views.generic import (
     UpdateView,
     DeleteView,
 )
+from django.conf import settings
+import os
 from .models import Transaction, Category
 from django.urls import reverse_lazy
+import matplotlib.pyplot as plt
+import matplotlib
+from django.db.models import Sum
+from financeiro.context_processors import soma_transacao
+
+matplotlib.use("Agg")
 
 
 class Homepage(TemplateView):
@@ -43,3 +51,44 @@ class DeleteTransacao(LoginRequiredMixin, DeleteView):
 class ListaDashboard(LoginRequiredMixin, ListView):
     model = Transaction
     template_name = "dashboard.html"
+
+
+# Função para criar o gráfico de barras
+def create_bar_chart():
+    # Chama a função soma_transacao, que retorna um contexto com informações de despesas e receitas
+    transacao_context = soma_transacao(
+        None
+    )  # Você pode passar None para request, pois não está usando em soma_transacao
+
+    # Obtém os valores de despesas e receitas a partir do contexto
+    soma_despesas = transacao_context["soma_despesas"]
+    soma_receitas = transacao_context["soma_receita"]
+
+    # Define os rótulos (labels) e os valores para o gráfico
+    labels = ["Despesas", "Receita"]
+    values = [soma_despesas, soma_receitas]
+
+    # Cria o gráfico de barras
+    plt.bar(labels, values)
+
+    # Adiciona rótulos e título ao gráfico
+    plt.xlabel("Tipos Despesas/Receita")
+    plt.ylabel("Valores")
+    plt.title("Gráfico de Barras Transações")
+
+    # Salva o gráfico como um arquivo de imagem PNG
+    plt.savefig("static/bar_chart.png")
+
+
+# Função que retorna a imagem do gráfico como resposta HTTP
+def chart_image(request):
+    # Chama a função para criar o gráfico
+    create_bar_chart()
+
+    # Define o caminho completo para a imagem gerada
+    image_path = os.path.join(settings.STATIC_ROOT, "bar_chart.png")
+
+    # Abre o arquivo de imagem em modo de leitura binária
+    with open(image_path, "rb") as f:
+        # Retorna a imagem como uma resposta HTTP com o tipo de conteúdo "image/png"
+        return HttpResponse(f.read(), content_type="image/png")
